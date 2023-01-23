98046106a7d1d547462daa28d89384882fe81c47
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-01-22 21:51:51 -0300

    Criando função de exibir game over e evento de reiniciar o site

--- JS/reconhecimentoDeVoz.js
+++ JS/reconhecimentoDeVoz.js
@@ -11,6 +11,7 @@ recognition.addEventListener('result', onSpeak)
 function onSpeak(evento) {
     chute = evento.results[0][0].transcript
     exibeChuteNaTela(chute)
+    exibeGameOver(chute)
     verificaSeOChutePossuiUmValorValido(chute)
 }
 
@@ -21,4 +22,18 @@ function exibeChuteNaTela(chute) {
     `
 }
 
-recognition.addEventListener('')
+function exibeGameOver(chute) {
+    if(chute.toUpperCase() === 'GAME OVER') {
+        document.body.innerHTML = `
+            <h2>GAME OVER</h2>
+            <h3>Você perdeu!!</3>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+
+        document.body.style.backgroundColor = '#CF0A0A'
+    }
+}
+
+recognition.addEventListener('end', () => recognition.start())
