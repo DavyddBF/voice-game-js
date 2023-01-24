ce114ca42a5b76a710a84e4dcd75daa516376c0f
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-01-24 13:17:29 -0300

    Documentando script

--- JS/reconhecimentoDeVoz.js
+++ JS/reconhecimentoDeVoz.js
@@ -1,20 +1,29 @@
+    // RECONHECIMENTO DE VOZ - CHAMADA DE FUNÇÕES E CONDIÇÕES
+
 const elementoChute = document.getElementById('chute')
 
+// Ativação do reconhecimento de voz
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
+// adicionando o reconhecimento numa constante
 const recognition = new SpeechRecognition();
+// mudando o idioma para português e iniciando ela na web
 recognition.lang = 'pt-br'
 recognition.start()
 
+// cria um evento que quando vir o resultado do que foi falado ele chama as funções de exibir itens na tela
 recognition.addEventListener('result', onSpeak)
 
+// função que chama outras funções que exibe coisas na tela
 function onSpeak(evento) {
+    // adiciona a variavel chute o que o usuario falou. Acessando o evento e pegando a fala no transcript
     chute = evento.results[0][0].transcript
     exibeChuteNaTela(chute)
     exibeGameOver(chute)
     verificaSeOChutePossuiUmValorValido(chute)
 }
 
+// recebe como parametro o chute ou o que o usuario disse e modifica a div chute adicionando outros elementos ao HTML
 function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
         <div>Você disse:</div>
@@ -22,8 +31,11 @@ function exibeChuteNaTela(chute) {
     `
 }
 
+// função que exibe o game over recebendo como parametro o que o usuario diz
 function exibeGameOver(chute) {
+    // condição. Verifica se o chute do usuário colocado em maiusculo com a função toUpperCase é igual a GAME OVER
     if(chute.toUpperCase() === 'GAME OVER') {
+        // se for true ele executa e modifica o body adicionando novos elementos e falando qual o número secreto e com um botão de reiniciar a partida
         document.body.innerHTML = `
             <h2>GAME OVER</h2>
             <h3>Você perdeu!!</3>
@@ -32,8 +44,10 @@ function exibeGameOver(chute) {
             <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
 
+        // mudança de cor de fundo do body
         document.body.style.backgroundColor = '#CF0A0A'
     }
 }
 
+// evento que quando o reconhecimento de voz terminar (end), ele ativa novamente dando um start()
 recognition.addEventListener('end', () => recognition.start())
--- JS/validacao.js
+++ JS/validacao.js
@@ -1,19 +1,32 @@
+    // VALIÇÃO DE FALA - VALIDA SE O QUE O USUÁRIO DISSE ESTÁ CORRETO OU ERRADO
+
+// função principal de verificação do que o usuário diz
 function verificaSeOChutePossuiUmValorValido() {
+    // adiciona o chute a uma variavel numero
     const numero = +chute
 
+    // chama a função chuteForValido recebendo como parametro o chute do usuario
     if(chuteForInvalido(numero)) {
+        // se der um NaN, ele modifica o HTML e adiciona a div(id="chute") uma nova div dizendo valor invalido
         elementoChute.innerHTML += '<div>Valor invalido</div>'
+        // retorna o resultado da função caso não ocorra nada
         return
     }
 
+    // chama a função que recebe como parametro o chute do usuario
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        // aqui avisa o usuario entre quais valores está o numero secreto
+        // modifica a div(id="chute") adicionando uma nova div falando que o valor está entre o menor e o maior valor
         elementoChute.innerHTML += `
         <div>Valor invalido: Fale um número entre ${menorValor} e ${maiorValor}</div>
         `
+        // retorna o resultado da função caso não ocorra nada
         return
     }
 
+    // verica se o chute do usuario é igual ao numero secreto salvo numa variavel que está no arquivo sortearNumero.js
     if (numero === numeroSecreto) {
+        // caso true, ele modifica o body adicionando informações como que o usuario acertou o numero secreto
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
@@ -21,26 +34,36 @@ function verificaSeOChutePossuiUmValorValido() {
             <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
+        // caso não seja igual ele verifica se o numero secreto esta maior ou menor. Verificando se o chute é maior que o numero secreto
+        // caso seja ele modifica a div(id="chute") e fiz se o valor é menor
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>
         `
     } else {
+        // caso o chute seja menor que o numero secreto ele executará essa condição
+        // se o chute for menor que o numero secreto ele da a dica que é um numero maior
         elementoChute.innerHTML += `
         <div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>
         `
     }
 }
 
+// função que retorna a condição para um if
 function chuteForInvalido(numero) {
+    // verifica se o chute é um numero
     return Number.isNaN(numero)
 }
 
+// função que retorna a condição para um if
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    // verifica se o chute é maior que o numero maximo ou se o chute é menor que o numero minimo
     return numero > maiorValor || numero < menorValor
 }
 
+// cria um evento que quando clicar aciona uma função
 document.body.addEventListener('click', evento => {
-    if(evento.target.id == 'jogar-novamente') {
+    // se o evento tiver um id igual a 'jogar-novamente' ele recarrega a pagina
+    if(evento.target.id === 'jogar-novamente') {
         window.location.reload()
     }
 })
